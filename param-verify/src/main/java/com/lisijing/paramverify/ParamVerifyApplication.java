cc3b9edafa2140b40fc71daf79f7424173200322
LiJianxin <844485538@qq.com>
2022-09-19 10:56:40 +0800

    feat：新增参数校验模板

---
+++ param-verify/src/main/java/com/lisijing/paramverify/ParamVerifyApplication.java
@@ -0,0 +1,13 @@
+package com.lisijing.paramverify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParamVerifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParamVerifyApplication.class, args);
+    }
+
+}
---
+++ param-verify/src/test/java/com/lisijing/paramverify/ParamVerifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisijing.paramverify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParamVerifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
